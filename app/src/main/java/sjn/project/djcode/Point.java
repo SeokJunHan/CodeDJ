aab1b4ad2e435a7a334b40ac2564ff6678f279fa
lje <dannyje31@gmail.com>
2019-12-09 16:08:00 +0900

    adapter 작업

---
+++ app/src/main/java/sjn/project/djcode/Point.java
@@ -0,0 +1,47 @@
+package sjn.project.djcode;
+
+public class Point {
+    private int pointImg;
+    private String pointName;
+    private String address;
+    private String pointPrice;
+
+    public Point(int pointImg, String pointName, String address, String pointPrice) {
+        this.pointImg = pointImg;
+        this.pointName = pointName;
+        this.address = address;
+        this.pointPrice = pointPrice;
+    }
+
+    public int getPointImg() {
+        return pointImg;
+    }
+
+    public void setPointImg(int pointImg) {
+        this.pointImg = pointImg;
+    }
+
+    public String getPointName() {
+        return pointName;
+    }
+
+    public void setPointName(String pointName) {
+        this.pointName = pointName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPointPrice() {
+        return pointPrice;
+    }
+
+    public void setPointPrice(String pointPrice) {
+        this.pointPrice = pointPrice;
+    }
+}
---
+++ app/src/main/java/sjn/project/djcode/PointAdapter.java
@@ -0,0 +1,41 @@
+package sjn.project.djcode;
+
+import android.content.Context;
+import android.graphics.Point;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.HashMap;
+
+public class PointAdapter extends ArrayAdapter<HashMap<Integer, Point>> {
+    private HashMap<Integer, Point> items;
+    Context context;
+
+    public PointAdapter(Context context, int resource, HashMap<Integer, Point> objects){
+        super(context, resource);
+        items = objects;
+        this.context = context;
+    }
+
+    public int getCount() { return items.size(); }
+
+    public View getView(int position,  View convertView,  ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        convertView = inflater.inflate(R.layout.point_listview, null, true);
+        ImageView imageView = convertView.findViewById(R.id.point_img);
+        TextView name = convertView.findViewById(R.id.point_name);
+        TextView address = convertView.findViewById(R.id.address);
+        TextView price = convertView.findViewById(R.id.price);
+        imageView.setImageResource(items.get(position).getPointImg());
+        name.setText(items.get(position).getPointName());
+        address.setText(items.get(position).getAddress());
+        price.setText(items.get(position).getPointPrice());
+        return convertView;
+    }
+}
+
+
